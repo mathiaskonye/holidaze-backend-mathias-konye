09fab7d47f348442e7f26b7476fb0c5f32cb2136
mathiaskonye <56630416+mathiaskonye@users.noreply.github.com>
2021-05-26 15:45:28 +0200

    api to git

---
+++ holidaze-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '529c7c08033d928020e404d9dc795754'),
+    },
+  },
+});
---
+++ holidaze-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '99e6f906-d1dd-43a9-984d-14fd5acdd9be'
+};
